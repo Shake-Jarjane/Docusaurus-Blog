388cc7d1ae535852652dbe79d9eda29d041785f4
Shake-Jarjane <2531535061@qq.com>
2023-01-26 20:43:45 +0800

    testing

--- static/baidu.php
+++ static/baidu.php
@@ -1,39 +1,21 @@
-<?php 
-
-$urls = array(
-
-    'https://www.shaking.site/',
-
-    'https://www.shaking.site/Hello-Blog',
-
-    'https://www.shaking.site/docs/Docusaurus-guides',
-
-    'https://www.shaking.site/about',
-
-);
-
+<?php
+{php}
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ?"https://": "http://";
+$url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+//获取当前访问目录的域名
+//  echo $url;
+$urls = array($url);
 $api = 'http://data.zz.baidu.com/urls?site=https://www.shaking.site&token=IxxtdEaM1M97FAsU';
-
 $ch = curl_init();
-
 $options =  array(
-
     CURLOPT_URL => $api,
-
     CURLOPT_POST => true,
-
     CURLOPT_RETURNTRANSFER => true,
-
     CURLOPT_POSTFIELDS => implode("\n", $urls),
-
     CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
-
 );
-
 curl_setopt_array($ch, $options);
-
 $result = curl_exec($ch);
-
-echo $result;
-
+echo "<script>console.log('当前百度推送$result;')</script>";
+{/php}
 ?>
