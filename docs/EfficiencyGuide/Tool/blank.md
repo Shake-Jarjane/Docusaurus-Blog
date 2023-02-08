ca7efb609efb121c4c01966ecc3ad806d49374d4
Shake-Jarjane <2531535061@qq.com>
2023-02-09 01:57:25 +0800

    wrote something

---
+++ docs/EfficiencyGuide/Tool/blank.md
@@ -0,0 +1,9 @@
+---
+id: Tool-blank
+slug: /Tool-blank
+title: blank
+date: 2023/02/01
+authors: Shake
+last_update:
+  date: 2023/02/01
+---
--- src/components/BrowserWindow2/index.tsx
+++ src/components/BrowserWindow2/index.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
 
 import styles from './styles.module.css';
 
-function BrowserWindow({children, url = 'http://localhost:3000'}) {
+interface Props {
+  children: ReactNode;
+  minHeight: number;
+  url: string;
+}
+
+export default function BrowserWindow({
+  children,
+  minHeight,
+  url = 'http://localhost:3000',
+}: Props): JSX.Element {
   return (
-    <div className={styles.browserWindow}>
+    <div className={styles.browserWindow} style={{minHeight}}>
       <div className={styles.browserWindowHeader}>
         <div className={styles.buttons}>
           <span className={styles.dot} style={{background: '#f25f58'}} />
           <span className={styles.dot} style={{background: '#fbbe3c'}} />
           <span className={styles.dot} style={{background: '#58cb42'}} />
         </div>
-        <div className={styles.browserWindowAddressBar}>
-          {/* {url} */}
+        <div className={clsx(styles.browserWindowAddressBar, 'text--truncate')}>
           <a href={url} target="_blank" >{url}</a>
         </div>
         <div className={styles.browserWindowMenuIcon}>
@@ -23,12 +40,8 @@ function BrowserWindow({children, url = 'http://localhost:3000'}) {
           </div>
         </div>
       </div>
-      <div className={styles.browserWindowBody}>
-        {children}
-      <span className={styles.cursor}></span>
-        </div>
+
+      <div className={styles.browserWindowBody}>{children}</div>
     </div>
   );
 }
-
-export default BrowserWindow;
